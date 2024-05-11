d12649c8db79a5c69b1e97367b02777f35097515
NguyenPhucLuan <baternguyen.2407@gmail.com>
2024-05-11 19:15:15 +0700

    luan

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import NavBarMenu from './components/NavBarMenu/NavBarMenu.jsx' ;
 
 function App() {
+  const menuItems = [
+    { name: "Trang chủ", link: "#" },
+    { name: "Sinh viên", link: "#" },
+    { name: "Giảng viên", link: "#" },
+    { name: "Phòng giáo vụ", link: "#" },
+    { name: "Đăng kí môn học", link: "#" },
+    { name: "Học phí", link: "#" }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div >
+      <Header></Header>
+      <NavBarMenu menuItems={menuItems}></NavBarMenu>
     </div>
   );
 }
