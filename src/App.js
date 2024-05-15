7e763d2b9e9361827bcaeecf8a6b799e4d807aab
phamducthangptit <pthang2506@gmail.com>
2024-05-15 21:13:41 +0700

    update vnpay

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HocPhi from './components/HocPhi/HocPhi';
+import HomePage from './components/TrangChu/HomePage';
+import Return from './components/HocPhi/ReturnPayment/Return';
+import XemDiem from './components/XemDiem/XemDiem';
+import ThongTinCaNhan from './components/ThongTinCaNhan/ThongTinCaNhan';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/hoc-phi" element={<HocPhi />} />
+        <Route path="*" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/xem-diem" element={<XemDiem />} />
+        <Route path="/return-payment" element={<Return />} />
+        <Route path="/thong-tin-ca-nhan" element={<ThongTinCaNhan/>} />
+      </Routes>
+    </Router>
   );
 }
 
